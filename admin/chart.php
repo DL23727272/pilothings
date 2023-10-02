458838f09d32ad14cb75a7ad5519bb84434e899b
DL23727272 <113783762+DL23727272@users.noreply.github.com>
2023-10-03 00:36:55 +0800

    updated admin page by Jeriel and modal button by DL

---
+++ admin/chart.php
@@ -0,0 +1,23 @@
+<?php
+include 'D:\xampp\htdocs\Appointment\Process\myConnection.php';
+
+$data = array(
+    'arwen' => getAppointmentCount($connect, 'arwen'),
+    'allen' => getAppointmentCount($connect, 'allen'),
+    'ramil' => getAppointmentCount($connect, 'ramil')
+);
+
+echo json_encode($data);
+
+function getAppointmentCount($connect, $barberName) {
+    $query = "SELECT COUNT(*) AS appointment_count FROM $barberName";
+    $result = mysqli_query($connect, $query);
+
+    if ($result) {
+        $row = mysqli_fetch_assoc($result);
+        return $row['appointment_count'];
+    } else {
+        return 0;
+    }
+}
+?>
--- admin/ramil.php
+++ admin/ramil.php
@@ -6,71 +6,59 @@
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" 
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-
+        
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" ></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
-    
-        <style>
-            table{
-                border-collapse: collapse;
-                border-spacing: 0;
-                width: 100%;
-                border: 1px solid #ddd;
-            }
-
-            th,td{
-                text-align: left;
-                padding: 16px;
-            }
-            tr:nth-child(even){
-                background-color: #f2f2f2;
-            }
-            img{
-                width: 100px;
-                height: 40px;
-            
-            }
-            a .btn{
-               margin: 2px;
-            }
-            h1{
-                text-align: center;
-            }
-            .btn a{
-                text-decoration: none;
-                color: white;
-            }
-        </style>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <!-- Alertify sakit sa ulo -->
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.min.css"/>
 
     </head>
     <body>
         <?php include ('../modals.php'); ?>
-        <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-            <a href="index.php" class="navbar-brand">Home</a>
-
-            <button class="navbar-toggler" type="button" data-target="#collapsibleNavbar">
-            <span class="navbar-toggler-icon"></span></button>
-            <div class="collapsse navbar-collapse" id="collapsibleNavbar">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                <button type="button" class="btn btn-success" data-toggle="modal" data-target="#addUser">Add User</button>
-                <button type="button" class="btn btn-success" data-toggle="modal"><a href="admin.php">Admin</a></button>
-                <button type="button" class="btn btn-success" data-toggle="modal"><a href="arwen.php">Arwen</a></button>
-                <button type="button" class="btn btn-success" data-toggle="modal"><a href="allen.php">Allen</a></button>
-                <button type="button" class="btn btn-success" data-toggle="modal"><a href="ramil.php">Ramil</a></button>
-            
-
+        <nav class="navbar bg-body-tertiary fixed-top shadow p-3 mb-5 bg-body-tertiary rounded">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="admin.php"><i class="fa-solid fa-users-line"></i> Admin</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Paesano Barber Shop</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                    <a class="nav-link" href="admin.php"><i class="fa-solid fa-users-line"></i> Admin</a>
                 </li>
-            </ul>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa-solid fa-plus"></i> Add User</a>
+                </li>
+                </ul>
+                <div class="btn-group">
+                    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa-solid fa-scissors"></i> Barbers
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="arwen.php"><i class="fa-solid fa-user-check"></i> Arwen</a></li>
+                        <li><a class="dropdown-item" href="allen.php"><i class="fa-solid fa-user-check"></i> Allen</a></li>
+                        <li><a class="dropdown-item" href="ramil.php"><i class="fa-solid fa-user-check"></i> Ramil</a></li>
+                    </ul>
+                </div>
+            </div>
+            </div>
         </div>
         </nav>
-       
+
 
         <br></br>
-        <h1>Ramil</h1>
+        <div class="container-fluid">
+        <h1 class="h4 mt-5"><i class="fa-solid fa-user-check"></i> Ramil</h1>
         <?php
-            include 'D:\xampp\htdocs\Appointment\Process\myConnection.php';
+           include 'D:\xampp\htdocs\Appointment\Process\myConnection.php';
             
             $currentDate = date("Y-m-d");
 
@@ -79,24 +67,22 @@
 
             $totalAppointmentsToday = mysqli_num_rows($result);
          ?>
-        <table>
+         <table class="table">
             <tr>
-                <th>Total appointment today: </th>
-                <th><?php echo $totalAppointmentsToday; ?></th>
-
-                </th>
+                <th scope="col">Total appointment today: </th>
+                <th scope="col"><i class='fa-solid fa-square-poll-vertical'></i> <?php echo $totalAppointmentsToday; ?></th>
             </tr>
             <tr>
-                <th> ID </th>
-                <th> Check-in Date </th>
-                <th> Check-in Time </th>
-                <th> Name </th>
-                <th> Type </th>
-                <th> Edit </th>
-                <th> Delete </th>
+                <th scope="col"> ID </th>
+                <th scope="col"> Check-in Date </th>
+                <th scope="col"> Check-in Time </th>
+                <th scope="col"> Name </th>
+                <th scope="col"> Type </th>
+                <th scope="col"> Edit </th>
+                <th scope="col"> Delete </th>
             </tr>
             <?php 
-                include 'D:\xampp\htdocs\Appointment\Process\myConnection.php';
+               include 'D:\xampp\htdocs\Appointment\Process\myConnection.php';
 
                 $query = "SELECT * FROM ramil";
                 $result = mysqli_query($connect, $query);
@@ -126,10 +112,17 @@
                     }
                 }
                 else{
-                    echo "0 results";
+                    echo "<p class='lead'><i class='fa-solid fa-square-poll-vertical'></i> 0 book</p>";
                 }
             ?>
             
         </table>
+        </div>
+        <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script>
+             alertify.set('notifier','position', 'top-right');
+             alertify.success('Welcome, Ramil!' );
+        </script>
     </body>
 </html>
